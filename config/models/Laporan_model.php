cd1a4f85f0a758083f1429d1679fad214b6a18b0
ArielDelvanza <97304810+ArielDelvanza@users.noreply.github.com>
2025-07-01 18:57:00 +0700

    Delete config/models directory

--- config/models/Laporan_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Laporan_model extends CI_model
-{
-
-  public function getAllLaporan()
-  {
-    return $this->db->get('tbl_hasil_diagnosa')->result_array();
-  }
-}
--- config/models/Member_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Member_model extends CI_model
-{
-  // menampilkan seluruh data gejala yang ada di tabel gejala.
-  public function Gejala()
-  {
-    return $this->db->get('tbl_gejala')->result_array();
-  }
-}
